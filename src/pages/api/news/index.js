51ac6b19a2fdf218313ff83e0b587e2ed2a8513a
strawman88 <158979989+strawman88@users.noreply.github.com>
2025-05-29 21:48:22 +1000

    layout changes, deletions etc

--- src/pages/api/news/index.js
+++ src/pages/api/news/index.js
@@ -1,11 +1,11 @@
-import { articles } from '../../../database/db';
+// import { articles } from '../../../database/db';
 
-function handler(req, res) {
-  if (req.method === "GET"){
-    res
-      .status(200)
-      .json(articles)
-  }
-}
+// function handler(req, res) {
+//   if (req.method === "GET"){
+//     res
+//       .status(200)
+//       .json(articles)
+//   }
+// }
 
-export default handler;
+// export default handler;
